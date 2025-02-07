ede7c9beba3c41e089a144226a6eeb7fb41237a9
Lorik Aliu <loriku123321@gmail.com>
2025-02-07 13:23:28 +0100

    added IUserService

---
+++ ExpenseTrackerAPI/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using ExpenseTrackerAPI.Models;
+
+namespace ExpenseTrackerAPI.Services
+{
+    public interface IUserService
+    {
+        Task<User> CreateUserAsync(User user);
+
+        Task<IEnumerable<User>> GetUsersAsync();
+
+        Task<User> GetUserByIdAsync(int id);
+
+        Task<User> UpdateUserAsync(User user);
+
+        Task<bool> DeleteUserAsync(int id);
+
+    }
+}
