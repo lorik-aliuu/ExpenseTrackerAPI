284103d3b9461ab5ada217a6a851cb65badce0c5
Lorik Aliu <loriku123321@gmail.com>
2025-02-07 12:41:19 +0100

    configured database connection and dependency injection

--- ExpenseTrackerAPI/Program.cs
+++ ExpenseTrackerAPI/Program.cs
@@ -1,6 +1,19 @@
+using ExpenseTrackerAPI.Data;
+using ExpenseTrackerAPI.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("LorikConnection")));
+
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
