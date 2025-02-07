bb9ab31a638b684272d925e1d6be7f2181d222d3
Lorik Aliu <loriku123321@gmail.com>
2025-02-07 11:49:37 +0100

    added ExpenseRepository

---
+++ ExpenseTrackerAPI/Data/Repositories/ExpenseRepository.cs
@@ -0,0 +1,54 @@
+﻿using ExpenseTrackerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerAPI.Data.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Expense> GetExpenseByIdAsync(int id)
+        {
+            return await _context.Expenses.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Expense>> GetAllExpensesAsync()
+        {
+            return await _context.Expenses.ToListAsync();
+        }
+
+        public async Task<Expense> AddExpenseAsync(Expense expense)
+        {
+            await _context.Expenses.AddAsync(expense);
+            await _context.SaveChangesAsync();
+            return expense;
+        }
+
+        public async Task<Expense> UpdateExpenseAsync(Expense expense)
+        {
+            _context.Expenses.Update(expense);
+            await _context.SaveChangesAsync();
+            return expense;
+        }
+
+        public async Task<bool> DeleteExpenseAsync(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if(expense == null)
+            {
+                return false;
+            }
+
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
---
+++ ExpenseTrackerAPI/Data/Repositories/IExpenseRepository.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTrackerAPI.Models;
+
+namespace ExpenseTrackerAPI.Data.Repositories
+{
+    public interface IExpenseRepository
+    {
+        Task<Expense> GetExpenseByIdAsync(int id);
+        Task<IEnumerable<Expense>> GetAllExpensesAsync();
+
+        Task<Expense> AddExpenseAsync(Expense expense);
+
+        Task<Expense> UpdateExpenseAsync(Expense expense);
+
+        Task<bool> DeleteExpenseAsync(int id);
+
+    }
+}
