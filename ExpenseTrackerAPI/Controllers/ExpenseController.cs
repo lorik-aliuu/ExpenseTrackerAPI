7ba89d28a049121357b1b0278f46fa125b3b14f8
Lorik Aliu <loriku123321@gmail.com>
2025-02-07 20:12:45 +0100

    added other endpoints for Expense Controller

--- ExpenseTrackerAPI/Controllers/ExpenseController.cs
+++ ExpenseTrackerAPI/Controllers/ExpenseController.cs
@@ -14,7 +14,7 @@ public class ExpenseController : ControllerBase
     }
 
   
-    [HttpPost]
+    [HttpPost("AddExpense")]
     public async Task<IActionResult> CreateExpense([FromBody] ExpenseDTO expenseDto)
     {
         if (expenseDto == null)
@@ -33,7 +33,7 @@ public class ExpenseController : ControllerBase
     }
 
  
-    [HttpGet("{id}")]
+    [HttpGet("GetExpenseById")]
     public async Task<IActionResult> GetExpenseById(int id)
     {
         var expense = await _expenseService.GetExpenseByIdAsync(id);
@@ -45,7 +45,7 @@ public class ExpenseController : ControllerBase
     }
 
   
-    [HttpGet("GetAll")]
+    [HttpGet("GetAllExpenses")]
     public async Task<IActionResult> GetAllExpenses()
     {
         var expenses = await _expenseService.GetAllExpensesAsync();
@@ -53,7 +53,7 @@ public class ExpenseController : ControllerBase
     }
 
  
-    [HttpPut("{id}")]
+    [HttpPut("UpdateById")]
     public async Task<IActionResult> UpdateExpense(int id, [FromBody] ExpenseDTO expenseDto)
     {
         if (expenseDto == null || expenseDto.Id != id)
@@ -66,7 +66,7 @@ public class ExpenseController : ControllerBase
     }
 
    
-    [HttpDelete("{id}")]
+    [HttpDelete("DeleteById")]
     public async Task<IActionResult> DeleteExpense(int id)
     {
         var expense = await _expenseService.GetExpenseByIdAsync(id);
@@ -77,5 +77,47 @@ public class ExpenseController : ControllerBase
 
         await _expenseService.DeleteExpenseAsync(id);
         return NoContent();
+    }
+
+    [HttpGet("get-most-expensive-expense")]
+    public async Task<IActionResult> GetMostExpensiveExpense()
+    {
+        var expense = await _expenseService.GetMostExpensiveExpenseAsync();
+        return Ok(expense);
+    }
+
+    [HttpGet("get-least-expensive")]
+    public async Task<IActionResult> GetLeastExpensiveExpense()
+    {
+        var expense = await _expenseService.GetLeastExpensiveExpenseAsync();
+        return Ok(expense);
+    }
+
+    [HttpGet("get-average-daily")]
+    public async Task<IActionResult> GetAverageDailyExpenses([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
+    {
+        var avgExpense = await _expenseService.GetAverageDailyExpensesAsync(fromDate, toDate);
+        return Ok(avgExpense);
+    }
+
+    [HttpGet("get-average-monthly")]
+    public async Task<IActionResult> GetAverageMonthlyExpenses([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
+    {
+        var avgExpense = await _expenseService.GetAverageMonthlyExpensesAsync(fromDate, toDate);
+        return Ok(avgExpense);
+    }
+
+    [HttpGet("get-average-yearly")]
+    public async Task<IActionResult> GetAverageYearlyExpenses([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
+    {
+        var avgExpense = await _expenseService.GetAverageYearlyExpensesAsync(fromDate, toDate);
+        return Ok(avgExpense);
+    }
+
+    [HttpGet("get-total-expenses")]
+    public async Task<IActionResult> GetTotalExpenses()
+    {
+        var totalExpenses = await _expenseService.GetTotalExpensesAsync();
+        return Ok(totalExpenses);
     }
 }
